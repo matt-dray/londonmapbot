710ad17f570f797f4fe6248ac274135d42fc7f85
Matt Dray <mwdray@gmail.com>
2020-09-28 13:18:13 +0100

    Extract and call script, set env objects

---
+++ londonmapbot-tweet.R
@@ -0,0 +1,31 @@
+# Create Twitter token
+londonmapbot_token <- rtweet::create_token(
+	app = "londonmapbot",
+	consumer_key = Sys.getenv("TWITTER_CONSUMER_API_KEY"),
+	consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
+	access_token = Sys.getenv("TWITTER_ACCESS_TOKEN"),
+	access_secret = Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+)
+
+# Generate random coordinates
+lon <- round(runif(1, -0.5, 0.27), 4)
+lat <- round(runif(1, 51.3, 51.7), 4)
+
+# Build URL and fetch from Mapbox API
+img_url <- paste0(
+	"https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/",
+	paste0(lon, ",", lat),
+	",15,0/600x400@2x?access_token=",
+	Sys.getenv("MAPBOX_PUBLIC_ACCESS_TOKEN")
+)
+
+# Download the image to a temporary location
+temp_file <- tempfile()
+download.file(img_url, temp_file)
+
+# Post the image to Twitter with the latlon as text
+rtweet::post_tweet(
+	status = paste0(lat, ", ", lon),
+	media = temp_file,
+	token = londonmapbot_token
+)
